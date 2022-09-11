632e278e6e6aea64742b524acaf2084906c0bfd7
DaviGuilherme <davi.paula@sptech.school>
2022-09-11 20:03:58 -0300

    prototipo de tela - login

---
+++ projeto-guardian-angel/src/main/java/com/mycompany/projeto/guardian/angel/prototipoTelas.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.projeto.guardian.angel;
+
+/**
+ *
+ * @author davig
+ */
+public class prototipoTelas {
+    public static void main(String[] args) {
+        System.out.println("cu");
+    }
+}
